a0101ae293d67e2afa43049509f4eebb03d7c553
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-02-11 09:37:50 +0000

    removed / from install files

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
         .then(cache => {cache.addAll([
-            '/index.html',
-            '/index.js',
-            '/main.css'
+            'index.html',
+            'index.js',
+            'main.css'
         ])})
         .then(() => {
             console.log('Service worker installed!')
