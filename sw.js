cac393484a71a5f4983d507a294d14d7f393c580
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-02-11 10:48:13 +0000

    added root to installed requests

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
         .then(cache => {cache.addAll([
+            '/',
             'index.html',
             'index.js',
             'main.css',
